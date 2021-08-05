1dcda7f84076878f984565812ed03e9898a514b3
jasba24 <johanbuitrago342@gmail.com>
2021-08-05 09:23:35 -0500

    move note collection model
    move the connection from the note collection to the database to another file

--- mongo.js
+++ mongo.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose')
-const { Schema, model } = mongoose
 
-const password = require('./password')
-
-const connectionString =
-`mongodb+srv://jasba24:${password}@cluster0.vdvkq.mongodb.net/midubootcamp?retryWrites=true&w=majority`
+const connectionString = process.env.MONGO_DB_URI
 
 // connection to mongodb
 mongoose.connect(connectionString, {
@@ -17,29 +13,3 @@ mongoose.connect(connectionString, {
     console.log('DB connected')
   })
   .catch(err => console.log(err))
-
-const noteSchema = new Schema({
-  content: String,
-  date: Date,
-  important: Boolean
-})
-
-const Note = model('Note', noteSchema)
-
-// Note.find({}).then(result => {
-//   console.log(result)
-//   mongoose.connection.close()
-// })
-
-const note = new Note({
-  content: 'MongoDB es increible, midu',
-  date: new Date(),
-  important: true
-})
-
-note.save()
-  .then((result) => {
-    console.log(result)
-    mongoose.connection.close()
-  })
-  .catch(err => console.error(err))
