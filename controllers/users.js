ca1e638863bc7ebf9f6da0de446dd07db845a8d6
jasba24 <johanbuitrago342@gmail.com>
2021-08-09 11:00:27 -0500

    Add method to add note data to user

--- controllers/users.js
+++ controllers/users.js
@@ -2,21 +2,45 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/User')
 
-usersRouter.post('/', async (req, res) => {
-  const { body } = req
-  const { username, name, password } = body
-
-  const passwordHash = await bcrypt.hash(password, 10)
-
-  const user = new User({
-    username,
-    name,
-    passwordHash
+usersRouter.get('/', async (req, res) => {
+  const users = await User.find({}).populate('notes', {
+    content: 1,
+    date: 1
   })
+  res.json(users)
+})
 
-  const savedUser = await user.save()
+usersRouter.post('/', async (req, res) => {
+  try {
+    const { body } = req
+    const { username, name, password } = body
 
-  res.status(201).json(savedUser)
+    const usersDB = await User.find({})
+    const users = await usersDB.map(u => u.toJSON())
+    const usernames = users.map(u => u.username)
+
+    const passwordHash = await bcrypt.hash(password, 10)
+
+    const user = new User({
+      username,
+      name,
+      passwordHash
+    })
+    if (usernames.includes(username)) {
+      res.status(400).send({ error: 'username to be unique' }).end()
+    } else {
+      const savedUser = await user.save()
+      res.status(201).json(savedUser)
+    }
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+usersRouter.delete('/:id', async (req, res) => {
+  const id = req.params.id
+  await User.findByIdAndRemove(id)
+  res.status(204).end()
 })
 
 module.exports = usersRouter
--- middleware/CastError.js
+++ middleware/CastError.js
@@ -1,5 +1,4 @@
 module.exports = (error, req, res, next) => {
-  console.log(error.name)
   if (error.name === 'CastError') {
     res.status(400).send({ error: 'Id used is malformed' })
   } else {
